448b28aa7be2fdcba96b31801b06d71de628d5ba
thaihuynhxyz <thaihuynhxyz@gmail.com>
2021-12-10 21:28:29 +0700

    Add repeated string

---
+++ src/hacker_rank/repeated_string/main.cpp
@@ -0,0 +1,114 @@
+#define CATCH_CONFIG_RUNNER
+
+#include <bits/stdc++.h>
+#include "catch2/catch.hpp"
+
+using namespace std;
+
+vector<string> split_string(string);
+
+/*
+ * Complete the 'repeatedString' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts following parameters:
+ *  1. STRING s
+ *  2. LONG_INTEGER n
+ */
+
+long repeatedString(string s, long n) {
+  const auto fullRepeated = n / s.size();
+  const auto lastStringSize = n - fullRepeated * s.size();
+  auto aCountFull = 0;
+  auto aCountLast = 0;
+  for (int i = 0; i < s.size(); i++) {
+    if (s[i] == 'a') {
+      if (i < lastStringSize) aCountLast++;
+      aCountFull++;
+    }
+  }
+  return aCountFull * fullRepeated + aCountLast;
+}
+
+void test_main(const string &input_file, const string &result_file) {
+  std::ifstream input(input_file);
+  std::cin.rdbuf(input.rdbuf());
+  ofstream fout(result_file);
+
+  string s;
+  getline(cin, s);
+
+  int n;
+  cin >> n;
+
+  int result = repeatedString(s, n);
+
+  fout << result << "\n";
+
+  fout.close();
+}
+
+long read(const string &filename) {
+  std::ifstream output(filename);
+  std::cin.rdbuf(output.rdbuf());
+  int result;
+  cin >> result;
+  cin.ignore(numeric_limits<streamsize>::max(), '\n');
+  return result;
+}
+
+TEST_CASE("Test case 0", "[single-file]") {
+  test_main("input00.txt", "result00.txt");
+
+  auto output = read("output00.txt");
+  auto result = read("result00.txt");
+
+  REQUIRE(result == output);
+}
+
+TEST_CASE("Test case 1", "[single-file]") {
+  test_main("input01.txt", "result01.txt");
+
+  auto output = read("output01.txt");
+  auto result = read("result01.txt");
+
+  REQUIRE(result == output);
+}
+
+int main(int argc, char *argv[]) {
+  // global setup...
+
+  int result = Catch::Session().run(argc, argv);
+
+  // global clean-up...
+  return result;
+}
+
+vector<string> split_string(string input_string) {
+  string::iterator new_end = unique(input_string.begin(), input_string.end(), [](const char &x, const char &y) {
+    return x == y and x == ' ';
+  });
+
+  input_string.erase(new_end, input_string.end());
+
+  while (input_string[input_string.length() - 1] == ' ') {
+    input_string.pop_back();
+  }
+
+  vector<string> splits;
+  char delimiter = ' ';
+
+  size_t i = 0;
+  size_t pos = input_string.find(delimiter);
+
+  while (pos != string::npos) {
+    splits.push_back(input_string.substr(i, pos - i));
+
+    i = pos + 1;
+    pos = input_string.find(delimiter, i);
+  }
+
+  splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+  return splits;
+}
