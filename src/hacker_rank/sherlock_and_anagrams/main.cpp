afc08a1761173daf49e7feb30730bb250221e476
thaihuynhxyz <thaihuynhxyz@gmail.com>
2021-12-11 13:22:04 +0700

    Add Sherlock and anagrams

---
+++ src/hacker_rank/sherlock_and_anagrams/main.cpp
@@ -0,0 +1,178 @@
+#define CATCH_CONFIG_RUNNER
+
+#include <bits/stdc++.h>
+#include "catch2/catch.hpp"
+
+using namespace std;
+
+vector<string> split_string(string);
+
+/*
+ * Complete the 'sherlockAndAnagrams' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING s as parameter.
+ */
+
+int sherlockAndAnagrams(string s) {
+  auto nCr = [](int n, int r) {
+    // p holds the value of n*(n-1)*(n-2)...,
+    // k holds the value of r*(r-1)...
+    long long p = 1, k = 1;
+ 
+    // C(n, r) == C(n, n-r),
+    // choosing the smaller value
+    r = min(n - r, r);
+ 
+    if (r != 0) {
+      while (r) {
+        p *= n;
+        k *= r;
+
+        // gcd of p, k
+        long long m = __gcd(p, k);
+
+        // dividing by gcd, to simplify
+        // product division by their gcd
+        // saves from the overflow
+        p /= m;
+        k /= m;
+
+        n--;
+        r--;
+      }
+ 
+      // k should be simplified to 1
+      // as C(n, r) is a natural number
+      // (denominator should be 1 ) .
+    }
+ 
+    // if our approach is correct p = ans and k =1
+    return p;
+  };
+  
+  unordered_map<string, int> map;
+  for (int size = 1; size < s.size() + 1; size++) {
+    for (int i = 0; i <= s.size() - size; i++) {
+      string str = s.substr(i, size);
+      sort(str.begin(), str.end());
+      map[str]++;
+    }
+  }
+  
+  long ans = 0;
+  for (const auto &entry : map) {
+    if (entry.second > 1) ans += nCr(entry.second, 2);
+  }
+  return ans;
+}
+
+void test_main(const string &input_file, const string &result_file) {
+  std::ifstream input(input_file);
+  std::cin.rdbuf(input.rdbuf());
+  ofstream fout(result_file);
+
+  int t;
+  cin >> t;
+  cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+  for (int i = 0; i < t; i++) {
+    string s;
+    getline(cin, s);
+
+    const auto result = sherlockAndAnagrams(s);
+
+    fout << result << endl;
+  }
+
+  fout.close();
+}
+
+vector<int> read(const string &filename) {
+  std::ifstream output(filename);
+  std::cin.rdbuf(output.rdbuf());
+
+  vector<int> result;
+  while (true) {
+    string str;
+    getline(cin, str);
+    if (str.empty()) break;
+
+    result.push_back(stoi(str));
+  }
+  return result;
+}
+
+TEST_CASE("Test case 0", "[single-file]") {
+  test_main("input00.txt", "result00.txt");
+
+  auto output = read("output00.txt");
+  auto result = read("result00.txt");
+
+  REQUIRE(result == output);
+}
+
+TEST_CASE("Test case 1", "[single-file]") {
+  test_main("input01.txt", "result01.txt");
+
+  auto output = read("output01.txt");
+  auto result = read("result01.txt");
+
+  REQUIRE(result == output);
+}
+
+TEST_CASE("Test case 2", "[single-file]") {
+  test_main("input02.txt", "result02.txt");
+
+  auto output = read("output02.txt");
+  auto result = read("result02.txt");
+
+  REQUIRE(result == output);
+}
+
+TEST_CASE("Test case 3", "[single-file]") {
+  test_main("input03.txt", "result03.txt");
+
+  auto output = read("output03.txt");
+  auto result = read("result03.txt");
+
+  REQUIRE(result == output);
+}
+
+int main(int argc, char *argv[]) {
+  // global setup...
+
+  int result = Catch::Session().run(argc, argv);
+
+  // global clean-up...
+  return result;
+}
+
+vector<string> split_string(string input_string) {
+  string::iterator new_end = unique(input_string.begin(), input_string.end(), [](const char &x, const char &y) {
+    return x == y and x == ' ';
+  });
+
+  input_string.erase(new_end, input_string.end());
+
+  while (input_string[input_string.length() - 1] == ' ') {
+    input_string.pop_back();
+  }
+
+  vector<string> splits;
+  char delimiter = ' ';
+
+  size_t i = 0;
+  size_t pos = input_string.find(delimiter);
+
+  while (pos != string::npos) {
+    splits.push_back(input_string.substr(i, pos - i));
+
+    i = pos + 1;
+    pos = input_string.find(delimiter, i);
+  }
+
+  splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+  return splits;
+}
