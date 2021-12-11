834795de1956959bc8f9e3cf40a1a878bfee120d
thaihuynhxyz <thaihuynhxyz@gmail.com>
2021-12-11 10:28:10 +0700

    Add two strings

---
+++ src/hacker_rank/two_strings/main.cpp
@@ -0,0 +1,129 @@
+#define CATCH_CONFIG_RUNNER
+
+#include <bits/stdc++.h>
+#include "catch2/catch.hpp"
+
+using namespace std;
+
+vector<string> split_string(string);
+
+/*
+ * Complete the 'twoStrings' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts following parameters:
+ *  1. STRING s1
+ *  2. STRING s2
+ */
+
+string twoStrings(string s1, string s2) {
+    unordered_map<char, int> map;
+    for (const auto &c : s1) map[c]++;
+    for (const auto &c : s2) {
+        if (map.count(c)) return "YES";
+    }
+    return "NO";
+}
+
+void test_main(const string &input_file, const string &result_file) {
+  std::ifstream input(input_file);
+  std::cin.rdbuf(input.rdbuf());
+  ofstream fout(result_file);
+
+  int t;
+  cin >> t;
+  cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+  for (int i = 0; i < t; i++) {
+    string s1;
+    getline(cin, s1);
+    string s2;
+    getline(cin, s2);
+
+    const auto result = twoStrings(s1, s2);
+
+    fout << result << endl;
+  }
+
+  fout.close();
+}
+
+vector<string> read(const string &filename) {
+  std::ifstream output(filename);
+  std::cin.rdbuf(output.rdbuf());
+
+  vector<string> result;
+  while (true) {
+    string str;
+    getline(cin, str);
+    if (str.empty()) break;
+
+    result.push_back(str);
+  }
+  return result;
+}
+
+TEST_CASE("Test case 0", "[single-file]") {
+  test_main("input00.txt", "result00.txt");
+
+  auto output = read("output00.txt");
+  auto result = read("result00.txt");
+
+  REQUIRE(result == output);
+}
+
+TEST_CASE("Test case 1", "[single-file]") {
+  test_main("input01.txt", "result01.txt");
+
+  auto output = read("output01.txt");
+  auto result = read("result01.txt");
+
+  REQUIRE(result == output);
+}
+
+TEST_CASE("Test case 2", "[single-file]") {
+  test_main("input02.txt", "result02.txt");
+
+  auto output = read("output02.txt");
+  auto result = read("result02.txt");
+
+  REQUIRE(result == output);
+}
+
+int main(int argc, char *argv[]) {
+  // global setup...
+
+  int result = Catch::Session().run(argc, argv);
+
+  // global clean-up...
+  return result;
+}
+
+vector<string> split_string(string input_string) {
+  string::iterator new_end = unique(input_string.begin(), input_string.end(), [](const char &x, const char &y) {
+    return x == y and x == ' ';
+  });
+
+  input_string.erase(new_end, input_string.end());
+
+  while (input_string[input_string.length() - 1] == ' ') {
+    input_string.pop_back();
+  }
+
+  vector<string> splits;
+  char delimiter = ' ';
+
+  size_t i = 0;
+  size_t pos = input_string.find(delimiter);
+
+  while (pos != string::npos) {
+    splits.push_back(input_string.substr(i, pos - i));
+
+    i = pos + 1;
+    pos = input_string.find(delimiter, i);
+  }
+
+  splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+  return splits;
+}
