3602ac7447283a1ee49cfaf20e64e68beeca9ec1
thaihuynhxyz <thaihuynhxyz@gmail.com>
2021-12-16 01:35:59 +0700

    Add common child

---
+++ src/hacker_rank/interview/strings/common_child/main.cpp
@@ -0,0 +1,127 @@
+#define CATCH_CONFIG_RUNNER
+
+#include <bits/stdc++.h>
+#include "catch2/catch.hpp"
+
+using namespace std;
+
+vector<string> split_string(string);
+
+/*
+ * Complete the 'commonChild' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. STRING s1
+ *  2. STRING s2
+ */
+
+int commonChild(string s1, string s2, int ans = 0) {
+  int n1 = s1.size(), n2 = s2.size();
+  vector<vector<int>> m(n1, vector<int>(n2, 0));
+  for (int i = 0; i < n1; i++) {
+    for (int j = 0; j < n2; j++) {
+      if (s1[i] == s2[j]) {
+        if (!i || !j) {
+          m[i][j] = 1;
+        } else {
+          m[i][j] = m[i - 1][j - 1] + 1;
+        }
+      } else {
+        m[i][j] = max(m[i][max(j - 1, 0)], m[max(i - 1, 0)][j]);
+      }
+    }
+  }
+  return m.back().back();
+}
+
+void test_main(const string &input_file, const string &result_file) {
+  std::ifstream input(input_file);
+  std::cin.rdbuf(input.rdbuf());
+  ofstream fout(result_file);
+
+  string a;
+  getline(cin, a);
+
+  string b;
+  getline(cin, b);
+
+  auto result = commonChild(a, b);
+
+  fout << result << "\n";
+
+  fout.close();
+}
+
+string read(const string &filename) {
+  std::ifstream output(filename);
+  std::cin.rdbuf(output.rdbuf());
+  string result;
+  getline(cin, result);
+  return result;
+}
+
+TEST_CASE("Test case 0", "[single-file]") {
+  test_main("input00.txt", "result00.txt");
+
+  auto output = read("output00.txt");
+  auto result = read("result00.txt");
+
+  REQUIRE(result == output);
+}
+
+TEST_CASE("Test case 1", "[single-file]") {
+  test_main("input01.txt", "result01.txt");
+
+  auto output = read("output01.txt");
+  auto result = read("result01.txt");
+
+  REQUIRE(result == output);
+}
+
+TEST_CASE("Test case 2", "[single-file]") {
+  test_main("input02.txt", "result02.txt");
+
+  auto output = read("output02.txt");
+  auto result = read("result02.txt");
+
+  REQUIRE(result == output);
+}
+
+int main(int argc, char *argv[]) {
+  // global setup...
+
+  int result = Catch::Session().run(argc, argv);
+
+  // global clean-up...
+  return result;
+}
+
+vector<string> split_string(string input_string) {
+  string::iterator new_end = unique(input_string.begin(), input_string.end(), [](const char &x, const char &y) {
+    return x == y and x == ' ';
+  });
+
+  input_string.erase(new_end, input_string.end());
+
+  while (input_string[input_string.length() - 1] == ' ') {
+    input_string.pop_back();
+  }
+
+  vector<string> splits;
+  char delimiter = ' ';
+
+  size_t i = 0;
+  size_t pos = input_string.find(delimiter);
+
+  while (pos != string::npos) {
+    splits.push_back(input_string.substr(i, pos - i));
+
+    i = pos + 1;
+    pos = input_string.find(delimiter, i);
+  }
+
+  splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+  return splits;
+}
