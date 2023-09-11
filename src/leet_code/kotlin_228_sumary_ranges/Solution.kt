29bdde0cf43d36367dbea0794a6d4e1228ae7026
thaihuynhxyz <thaihuynhxyz@gmail.com>
2023-09-11 16:24:22 +0700

    kotlin_228_sumary_ranges

---
+++ src/leet_code/kotlin_228_sumary_ranges/Solution.kt
@@ -0,0 +1,21 @@
+package leet_code.kotlin_228_sumary_ranges
+
+class Solution {
+    fun summaryRanges(nums: IntArray): List<String> {
+        val ans = mutableListOf<String>()
+        var i = 0
+        while (i < nums.size) {
+            var j = i
+            while (j + 1 < nums.size && nums[j + 1] == nums[j] + 1) {
+                j++
+            }
+            if (i == j) {
+                ans.add(nums[i].toString())
+            } else {
+                ans.add("${nums[i]}->${nums[j]}")
+            }
+            i = j + 1
+        }
+        return ans
+    }
+}
---
+++ src/leet_code/kotlin_228_sumary_ranges/SolutionTest.kt
@@ -0,0 +1,18 @@
+package leet_code.kotlin_228_sumary_ranges
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class SolutionTest {
+    @Test
+    fun test() {
+        Assertions.assertEquals(
+            listOf("0->2", "4->5", "7"),
+            Solution().summaryRanges(intArrayOf(0, 1, 2, 4, 5, 7)).sorted()
+        )
+        Assertions.assertEquals(
+            listOf("0", "2->4", "6", "8->9"),
+            Solution().summaryRanges(intArrayOf(0, 2, 3, 4, 6, 8, 9)).sorted()
+        )
+    }
+}
